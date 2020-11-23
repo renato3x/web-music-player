4064b12554515201eaf8e65121936b9e0be48901
renatodev17 <pereirarenato21@gmail.com>
2020-11-23 18:35:31 -0300

    adding playlist and Creating class MusicPlayer

---
+++ public/js/index.js
@@ -0,0 +1,20 @@
+const tracks = [
+  {
+    title: 'About You',
+    author: 'Ascence',
+    thumb: '../public/img/about_you.jpg',
+    audio: '../public/audio/about_you.mp3'
+  },
+  {
+    title: 'Ark',
+    author: 'ship Wrek & Zookeepers',
+    thumb: '../public/img/ark.jpg',
+    audio: '../public/audio/ark.mp3'
+  },
+  {
+    title: 'Ride',
+    author: 'Clarx',
+    thumb: '../public/img/ride.jpg',
+    audio: '../public/audio/ride.mp3'
+  }
+]
